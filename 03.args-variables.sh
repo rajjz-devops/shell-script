60559ba594cc54285f9662759e225202ad9c29fb
raju.gaddam <raju.qa32@gmail.com>
2025-01-02 00:04:38 -0500

    arguments example

---
+++ 03.args-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ACTOR1=$1
+ACTOR2=$2
+
+echo "$ACTOR1:: how are you doing $ACTOR2"
+echo "$ACTOR2:: I am fine $ACTOR1"
+echo "$ACTOR1:: Finished off your breakfast"
+echo "$ACTOR2::yeah $ACTOR1. going out to play."
---
+++ 04.trycommit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+$CHECK1=$1
+$CHECK2=$2
+
+echo "Is $CHECK1 equals $CHECK2"
