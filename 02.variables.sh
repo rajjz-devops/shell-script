e35b97b41d2092a5bd75c9745a19b6fdf3799e07
raju.gaddam <raju.qa32@gmail.com>
2025-01-01 23:51:16 -0500

    trying to do all in one stmt

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Mother:: how are you doing beta"
-echo "Son:: I am fine amma"
-echo "Mother:: Finished off your breakfast"
-echo "Son::yeah amma. going out to play."
+ACTOR1=Mother
+ACTOR2=Son
+
+echo "$ACTOR1:: how are you doing $ACTOR2"
+echo "$ACTOR2:: I am fine $ACTOR1"
+echo "$ACTOR1:: Finished off your breakfast"
+echo "$ACTOR2::yeah $ACTOR1. going out to play."
