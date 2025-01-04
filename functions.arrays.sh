7e54265851744e769e9bfc8f0646ae0437a1d971
raju.gaddam <raju.qa32@gmail.com>
2025-01-03 21:43:22 -0500

    arguments example

---
+++ functions.arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+groceries = ("eggs","milk","rice","butter","quinoa")
+
+echo "first grocery in the list :: ${groceries[0]}"
+echo "second grocery in the list :: ${groceries[3]}"
+
+#-gt, -lt, -ne, -eq, -ge, -le
+
+
